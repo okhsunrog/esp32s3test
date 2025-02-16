5a6e74517559641115ea703c8b26d919635488ea
Danila Gornushko <me@okhsunrog.dev>
2025-02-16 04:01:21 +0000

    wip: adding ws2812b

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use anyhow::Result;
-use esp_idf_svc::hal::{prelude::*, spi};
+use esp_idf_svc::hal::{gpio::AnyIOPin, prelude::*, spi::*};
+use smart_leds::SmartLedsWriteAsync;
+use smart_leds::{brightness, RGB8};
+use ws2812_async::{Grb, Ws2812};
+
+const NUM_LEDS: usize = 20;
 
 fn main() -> Result<()> {
     esp_idf_svc::sys::link_patches();
@@ -7,6 +12,17 @@ fn main() -> Result<()> {
     let p = Peripherals::take()?;
     let spi = p.spi2;
     let led_pin = p.pins.gpio10;
+
+    // do I need to configure it in any special way for DMA to work?
+    let driver = SpiDriver::new_without_sclk::<SPI2>(
+        spi,
+        led_pin,
+        AnyIOPin::none(),
+        &SpiDriverConfig::new(),
+    )?;
+    let config = config::Config::new().baudrate(3200.kHz().into());
+    let mut device = SpiDeviceDriver::new(&driver, Some(cs_1), &config_1)?;
+    let mut ws: Ws2812<_, Grb, { 12 * NUM_LEDS }> = Ws2812::new(device);
 
     log::info!("Hello, world!");
     Ok(())
