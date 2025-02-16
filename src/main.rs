311c28dd1dba3c1a864ee45830822eb2c3a2a3ac
Danila Gornushko <me@okhsunrog.dev>
2025-02-16 03:38:09 +0000

    adding spi and crates for ws2812

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
-fn main() {
+use anyhow::Result;
+use esp_idf_svc::hal::{prelude::*, spi};
+
+fn main() -> Result<()> {
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
+    let p = Peripherals::take()?;
+    let spi = p.spi2;
+    let led_pin = p.pins.gpio10;
+
     log::info!("Hello, world!");
+    Ok(())
 }
