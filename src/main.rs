bde91eb8fc8510567cc581bc037e6a87277f8c83
okhsunrog <me@okhsunrog.dev>
2025-02-17 14:04:25 +0300

    fixed compilation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use std::time;
+
 use anyhow::Result;
 use esp_idf_svc::hal::{gpio::AnyIOPin, prelude::*, spi::*};
-use smart_leds::SmartLedsWriteAsync;
+use smart_led_effects::{
+    strip::{self, EffectIterator},
+    Srgb,
+};
 use smart_leds::{brightness, RGB8};
 use ws2812_async::{Grb, Ws2812};
-use embedded_hal_async::spi::SpiBus;
 
 const NUM_LEDS: usize = 20;
 
@@ -12,19 +16,43 @@ fn main() -> Result<()> {
     esp_idf_svc::log::EspLogger::initialize_default();
     let p = Peripherals::take()?;
     let spi = p.spi2;
-    let led_pin = p.pins.gpio10;
+    let led_pin = p.pins.gpio21;
 
     // do I need to configure it in any special way for DMA to work?
-    let mut driver: SpiDriver<'_> = SpiDriver::new_without_sclk::<SPI2>(
+    let driver: SpiDriver<'_> = SpiDriver::new_without_sclk::<SPI2>(
         spi,
         led_pin,
         AnyIOPin::none(),
         &SpiDriverConfig::new(),
     )?;
     let config = config::Config::new().baudrate(3200.kHz().into());
-    let mut device = SpiBusDriver::new(&mut driver, &config)?;
-    let mut ws: Ws2812<_, Grb, { 12 * NUM_LEDS }> = Ws2812::new(&mut device);
+    let device = SpiBusDriver::new(driver, &config)?;
+    let _ws: Ws2812<_, Grb, { 12 * NUM_LEDS }> = Ws2812::new(device);
+    let mut _data = [RGB8::default(); NUM_LEDS];
+
+    // loop {
+    //     for j in 0..(256 * 5) {
+    //         for i in 0..NUM_LEDS {
+    //             data[i] = wheel((((i * 256) as u16 / NUM_LEDS as u16 + j as u16) & 255) as u8);
+    //         }
+    //         ws.write(brightness(data.iter().cloned(), 32)).await.ok();
+    //         time::sleep(time::Duration::from_millis(10)).await;
+    //     }
+    // }
 
     log::info!("Hello, world!");
     Ok(())
+}
+
+fn wheel(mut wheel_pos: u8) -> RGB8 {
+    wheel_pos = 255 - wheel_pos;
+    if wheel_pos < 85 {
+        return (255 - wheel_pos * 3, 0, wheel_pos * 3).into();
+    }
+    if wheel_pos < 170 {
+        wheel_pos -= 85;
+        return (0, wheel_pos * 3, 255 - wheel_pos * 3).into();
+    }
+    wheel_pos -= 170;
+    (wheel_pos * 3, 255 - wheel_pos * 3, 0).into()
 }
