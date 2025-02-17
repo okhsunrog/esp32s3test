dfda8ac222f1b87af2582263916cfbe2133b5285
okhsunrog <me@okhsunrog.dev>
2025-02-17 12:29:59 +0300

    trying to fix spi bus

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use esp_idf_svc::hal::{gpio::AnyIOPin, prelude::*, spi::*};
 use smart_leds::SmartLedsWriteAsync;
 use smart_leds::{brightness, RGB8};
 use ws2812_async::{Grb, Ws2812};
+use embedded_hal_async::spi::SpiBus;
 
 const NUM_LEDS: usize = 20;
 
@@ -14,15 +15,15 @@ fn main() -> Result<()> {
     let led_pin = p.pins.gpio10;
 
     // do I need to configure it in any special way for DMA to work?
-    let driver = SpiDriver::new_without_sclk::<SPI2>(
+    let mut driver: SpiDriver<'_> = SpiDriver::new_without_sclk::<SPI2>(
         spi,
         led_pin,
         AnyIOPin::none(),
         &SpiDriverConfig::new(),
     )?;
     let config = config::Config::new().baudrate(3200.kHz().into());
-    let mut device = SpiDeviceDriver::new(&driver, Some(cs_1), &config_1)?;
-    let mut ws: Ws2812<_, Grb, { 12 * NUM_LEDS }> = Ws2812::new(device);
+    let mut device = SpiBusDriver::new(&mut driver, &config)?;
+    let mut ws: Ws2812<_, Grb, { 12 * NUM_LEDS }> = Ws2812::new(&mut device);
 
     log::info!("Hello, world!");
     Ok(())
